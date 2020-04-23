f418520c51f7d654f5463672311a8ed6d602f38b
Joey Wardenaar <wardenj@gmail.com>
2020-04-22 17:13:03 -0700

    it works
    everything is running as it should

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ import constants
 import copy
 import statistics
 
+
 players_copy = copy.deepcopy(constants.PLAYERS)
 teams_copy = copy.deepcopy(constants.TEAMS)
 experienced_players = []
@@ -26,7 +27,7 @@ def height_splitter():
         player['height'] = int(player['height'].split(' ')[0])
 
 
-def guardian_string():
+def guardian_to_string():
     for player in players_copy:
         player['guardians'] = player['guardians'].replace(' and', ',')
 
@@ -48,17 +49,99 @@ def team_builder(team1, team2, team3):
         else:
             team3.append(noobs.pop())
 
+
 def team_height(team):
     all_heights = [player['height'] for player in team]
-    return statistics.mean(all_heights)
+    return round(statistics.mean(all_heights))
 
 
+def display_players(team):
+    player_string = ', '.join([player['name'] for player in team])
+    return player_string
 
-experience_check()
-height_splitter()
-guardian_string()
-team_builder(panthers, bandits, warriors)
-print(panthers)
-print(bandits)
-print(warriors)
-team_height(panthers)
+
+def roster_size(team):
+    total_size = len(team)
+    experienced = 0
+    for player in team:
+        if player['experience'] is True:
+            experienced += 1
+        else:
+            pass
+    return [total_size, experienced, (total_size - experienced)]
+
+
+def team_guardians(team):
+    guardian_string = ', '.join([player['guardians'] for player in team])
+    return guardian_string
+
+
+def teams_stats_display(team, index):
+    print(f"""
+    {teams_copy[index].upper()}
+    {'-' * 40}
+    Players: {display_players(team)}
+    Total Players: {roster_size(team)[0]}
+    Experienced Players: {roster_size(team)[1]}
+    Players Without Experience: {roster_size(team)[2]}
+    Average Height of Players: {team_height(team)} inches
+    Player's Guardians: {team_guardians(team)}
+    """)
+
+
+def display_data():
+    heading = f"""
+    WELCOME TO THE BASKETBALL STATS TOOL
+    {'-' * 40}
+    MAIN MENU:
+    """
+    print(heading)
+    input("Press Enter to continue")
+    team_display = f"""
+    {'-' * 40}
+    1) {teams_copy[0]}
+    2) {teams_copy[1]}
+    3) {teams_copy[2]}
+    4) Exit Program
+    {'-' * 40}
+    """
+    print(team_display)
+
+    while True:
+        team_select = input("Enter the number that corresponds to the team you would like to view or enter 4 "
+                            "to exit   ")
+
+        try:
+            team_select = int(team_select)
+            if team_select not in range(1, 5):
+                raise ValueError
+
+        except ValueError:
+            print("that answer does not correspond to one of our teams")
+            continue
+
+        if team_select == 1:
+            teams_stats_display(panthers, 0)
+            input("Press Enter to continue")
+            print(team_display)
+
+        elif team_select == 2:
+            teams_stats_display(bandits, 1)
+            input("Press Enter to continue")
+            print(team_display)
+
+        elif team_select == 3:
+            teams_stats_display(warriors, 2)
+            input("Press Enter to continue")
+            print(team_display)
+
+        else:
+            break
+
+
+if __name__ == "__main__":
+    experience_check()
+    height_splitter()
+    guardian_to_string()
+    team_builder(panthers, bandits, warriors)
+    display_data()
